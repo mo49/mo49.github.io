cd253c1592f82f76b427461cf9f39d4b3b9f4153
mo49 <k.mochizuki.0409@gmail.com>
2019-03-27 19:25:16 +0900

    大枠

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,20 @@
 <template>
   <section class="container">
     <div>
-      <h1 class="title">
-        PORTFOLIO SITE
-      </h1>
+      <h1 class="title">PORTFOLIO SITE</h1>
+
+      <h2>PROFILE</h2>
+      <p>photo.</p>
+      <p>name.</p>
+
+      <h2>HISTORY</h2>
+      <h3>CAREER</h3>
+      <h3>EDUCATION</h3>
+
+      <h2>GALLERY</h2>
       <gallery />
+
+      <h2>SOCIAL ACCOUNTS</h2>
       <social-accounts />
     </div>
   </section>
