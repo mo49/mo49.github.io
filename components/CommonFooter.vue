879fb4eea46b4ee7842fad174dd6216fca7ea7e6
mo49 <k.mochizuki.0409@gmail.com>
2019-03-27 16:22:13 +0900

    scss読み込んで動かしてみるまで

---
+++ components/CommonFooter.vue
@@ -0,0 +1,14 @@
+<template>
+  <footer class="footer">
+    <h1>footer</h1>
+    <ul>
+      <li>hoge</li>
+      <li>huga</li>
+      <li>fuga</li>
+    </ul>
+  </footer>
+</template>
+
+<style lang="scss">
+@import "~assets/sass/module/footer";
+</style>
---
+++ components/CommonHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <header class="header">
+    <h1>header</h1>
+  </header>
+</template>
+
+<style lang="scss">
+@import "~assets/sass/module/header";
+</style>
