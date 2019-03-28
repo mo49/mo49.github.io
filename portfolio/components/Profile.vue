ff9e10646c7a9d65dbc5117c13a75205bce819af
mo49 <k.mochizuki.0409@gmail.com>
2019-03-28 17:38:09 +0900

    update profile

--- portfolio/components/Profile.vue
+++ portfolio/components/Profile.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="profile">
     <h2>PROFILE</h2>
-    <p>photo.</p>
-    <p>name.</p>
+    <div class="profile__content">
+      <div class="profile__left">
+        <p class="profile__thumb"></p>
+      </div>
+      <div class="profile__right">
+        <p class="profile__name">Kazuki Mochizuki | 1989.04.09</p>
+        <p class="profile__desc">Developer / Otaku / Backpacker</p>
+      </div>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+@import "~assets/sass/module/profile";
+.profile__thumb{
+  background-image: url("~assets/image/profile2.png");
+}
+</style>
+
