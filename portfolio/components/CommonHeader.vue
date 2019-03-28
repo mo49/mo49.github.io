5402f4e886e7c40b91cbbb36bb60b3f63d8ce7fb
mo49 <k.mochizuki.0409@gmail.com>
2019-03-28 15:05:25 +0900

    header logo

--- portfolio/components/CommonHeader.vue
+++ portfolio/components/CommonHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
     <nuxt-link :to="{name: 'index'}">
-      <h1>logo</h1>
+      <h1 class="header__logo">logo</h1>
     </nuxt-link>
 
   </header>
@@ -9,4 +9,7 @@
 
 <style lang="scss">
 @import "~assets/sass/module/header";
+.header__logo{
+  background-image: url("~assets/image/logo.png");
+}
 </style>
