b04be56076e15dd96e54c289304a3da5a641bdde
mo49 <k.mochizuki.0409@gmail.com>
2019-03-29 15:37:23 +0900

    snsにリンク貼る

--- portfolio/components/SocialAccounts.vue
+++ portfolio/components/SocialAccounts.vue
@@ -2,10 +2,27 @@
   <div class="social-accounts">
     <h2>SOCIAL ACCOUNTS</h2>
     <!-- <font-awesome-icon icon="coffee" /> -->
-    <font-awesome-icon :icon="['fab', 'facebook']" class="fa-2x"/>
-    <font-awesome-icon :icon="['fab', 'twitter-square']" class="fa-2x"/>
-    <font-awesome-icon :icon="['fab', 'instagram']" class="fa-2x"/>
-    <font-awesome-icon :icon="['fab', 'github']" class="fa-2x"/>
-    <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-2x"/>
+    <a href="https://www.facebook.com/kazuki.mochizuki" target="_blank">
+      <font-awesome-icon :icon="['fab', 'facebook']" class="fa-3x"/>
+    </a>
+    <a href="https://twitter.com/mczkzk" target="_blank">
+      <font-awesome-icon :icon="['fab', 'twitter-square']" class="fa-3x"/>
+    </a>
+    <a href="https://github.com/mo49" target="_blank">
+      <font-awesome-icon :icon="['fab', 'github']" class="fa-3x"/>
+    </a>
+    <a href="https://qiita.com/mczkzk" target="_blank">
+      <font-awesome-icon icon="blog" class="fa-3x"/>
+    </a>
+    <a href="https://www.linkedin.com/in/kazuki-mochizuki" target="_blank">
+      <font-awesome-icon :icon="['fab', 'linkedin']" class="fa-3x"/>
+    </a>
+    <a href="https://www.youtube.com/channel/UCqCO5VLG_VxhR3tQGhD_exQ" target="_blank">
+      <font-awesome-icon :icon="['fab', 'youtube-square']" class="fa-3x"/>
+    </a>
+    <a href="https://www.instagram.com/mczkzk_/" target="_blank">
+      <font-awesome-icon :icon="['fab', 'instagram']" class="fa-3x"/>
+    </a>
+
   </div>
 </template>
