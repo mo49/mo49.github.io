610109649e22adc043fd755732358f9a3cc01f8a
mo49 <k.mochizuki.0409@gmail.com>
2019-03-29 14:47:23 +0900

    update viewport

--- portfolio/nuxt.config.js
+++ portfolio/nuxt.config.js
@@ -6,7 +6,7 @@ module.exports = {
     title: 'portfolio',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' },
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
