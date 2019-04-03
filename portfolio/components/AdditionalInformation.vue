fa1b5f000d0544ad0072c6b9d6f376a9d6f480b0
mo49 <k.mochizuki.0409@gmail.com>
2019-04-03 14:29:04 +0900

    update additional-information

--- portfolio/components/AdditionalInformation.vue
+++ portfolio/components/AdditionalInformation.vue
@@ -4,14 +4,37 @@
     <div class="additional-information__content">
       <ul class="additional-information__list">
         <li class="additional-information__item">
-          <a href="https://www.youracclaim.com/badges/2d19f647-3f90-47ba-84d9-3ac3a941ec84/" target="_blank">Unity Certified Developer</a>
+          <p>・
+            <a href="https://www.youracclaim.com/badges/2d19f647-3f90-47ba-84d9-3ac3a941ec84/" target="_blank">Unity Certified Developer</a>
+          </p>
         </li>
         <li class="additional-information__item">
-          <p>Japanese - Native, English - Basic</p>
+          <p>・Japanese
+            - Native, English
+            - Basic</p>
         </li>
         <li class="additional-information__item">
-          <p>Travel</p>
-          <p>America, china</p>
+          <p>・Travel Abroad</p>
+          <p>
+            - China(Beijing, Shanghai, Xi'an, Shenzhen, Hong Kong)<br>
+            - Korea(Seoul)<br>
+            - Philippine(Cebu Island)<br>
+            - Thailand(Bangkok, Ayutthaya)<br>
+            - India(New Delhi, Jaipur, Agra, Haridwar)<br>
+            - France(Paris, Mont Saint-Michel)<br>
+            - UK(London)<br>
+            - Italy(Roma)<br
+            >- Spain(Madrid, Barcelona)<br>
+            - Netherlands(Amsterdam, Rotterdam, Alkmaar)<br>
+            - Egypt(Cairo, Giza, Bahariya, Luxor, Aswan, Abu Simbel)<br>
+            - Jordan(Amman, Petra)<br>
+            - Israel(Jerusalem)<br>
+            - USA(NY, Washington D.C., LA, Los Angeles, Hawaii)<br>
+            - Canada(Banff)<br>
+            - Mexico(Mexico City)<br>
+            - Peru(Lima, Cusco, Huacachina, Nazca, Ollantaytambo, Machu Picchu, Puno)<br>
+            - Chile(Santiago, Easter Island)
+          </p>
         </li>
       </ul>
     </div>
