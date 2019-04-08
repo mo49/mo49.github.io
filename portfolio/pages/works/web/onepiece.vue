4914a876c7364bb2ce19d4d24fda135551629486
mo49 <k.mochizuki.0409@gmail.com>
2019-04-08 13:27:04 +0900

    onepiece 完成

---
+++ portfolio/pages/works/web/onepiece.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="container work" data-page="work">
+    <h1 class="work__title">
+      ONE PIECE WCI編 テレビアニメ連動サイト
+    </h1>
+    <div class="work-about" data-bg="odd">
+      <h2>About</h2>
+      <p>
+        『ワンピース』のテレビアニメ放送とスマホがリアルタイムで連動する12週間連続企画。アニメ放送中に、「アツイ！」と思ったシーンに合わせて連打！一放送を60シーンに分割した中で、自分が最も連打したシーンとみんなが最も連打したシーンが一致すると、プレゼント応募券を獲得できるというサイト。<br>
+        <br>
+        私の担当はオンエア画面と結果画面。全体参加人数、シーンごとの全員のタップ数と個人のタップ数をリアルタイムに表示し連動感を演出した。インタラクティブかつスケーラブルなグラフを描画するためにD3.jsを使用。オンエア中の随所にはさまれる割り込みアニメーションや結果画面のスロット、紙吹雪の演出などアニメーションもりもり。各放送回のトンマナにより7パターンのデザイン変更もある。
+      </p>
+
+    </div>
+    <div class="work-image" data-bg="even">
+      <h2>Image / Movie</h2>
+      <ul class="work-image__list">
+        <li class="work-image__item">
+          <img src="~assets/image/works/web/onepiece/1.png" @click="openModal">
+          <caption>
+            オンエア画面
+          </caption>
+        </li>
+        <li class="work-image__item">
+          <img src="~assets/image/works/web/onepiece/2.png" @click="openModal">
+          <caption>
+            結果画面
+          </caption>
+        </li>
+        <li class="work-image__item">
+          <img src="~assets/image/works/web/onepiece/3.jpg" @click="openModal">
+          <caption>
+            ジャンプ誌面掲載
+          </caption>
+        </li>
+        <li class="work-image__item size-full">
+          <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/8st9LbcYIHQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <caption>実際に遊んでみている様子</caption>
+        </li>
+        <li class="work-image__item size-full">
+          <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/nYvS4oDFHlo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <caption>画面キャプチャ</caption>
+        </li>
+        <li class="work-image__item size-full">
+          <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/Eqf75kwe1CM?start=1305" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <caption>特番でスタッフさんに触ってみていただいている様子</caption>
+        </li>
+      </ul>
+    </div>
+    <div class="work-skill" data-bg="odd">
+      <h2>Skill</h2>
+      <p>D3.js / JavaScript / Canvas</p>
+    </div>
+    <div class="work-time" data-bg="even">
+      <h2>Production Time</h2>
+      <p>from September to October, 2017. (about 2months)</p>
+    </div>
+    <div class="work-media" data-bg="odd">
+      <h2>Media</h2>
+      <ul>
+        <li><a href="https://www.kayac.com/news/2017/09/ONEPIECE_1709" target="_blank">史上初！？『ワンピース』アニメ放送とスマホがリアルタイムで連動する、特設プレゼントキャンペーンがスタート！</a></li>
+        <li><a href="https://www.kayac.com/service/client/1576" target="_blank">ONE PIECE（ワンピース）アニメ連動キャンペーン</a></li>
+        <li><a href="http://www.animatetimes.com/news/details.php?id=1506090757" target="_blank">『ワンピース』アニメ放送とスマホがリアルタイムで連動する、特設プレゼントキャンペーンサイトがオープン！</a></li>
+        <li><a href="http://news.mynavi.jp/news/2017/09/24/033/" target="_blank">TVアニメ『ワンピース』、スマホとリアルタイムで連動する特設サイト開設</a></li>
+      </ul>
+    </div>
+    <modal ref="modal" />
+  </section>
+</template>
+
+<script>
+import Modal from "~/components/Modal"
+
+export default {
+  components: {
+    Modal,
+  },
+  methods: {
+    openModal(evt) {
+      this.$refs.modal.openModal(evt);
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import "~assets/sass/page/works";
+</style>
+
