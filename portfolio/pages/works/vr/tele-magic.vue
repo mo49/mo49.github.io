5f8823d7b553be3bca7249cda3502786d7848319
mo49 <k.mochizuki.0409@gmail.com>
2019-04-08 11:52:30 +0900

    tele-magic 完成

--- portfolio/pages/works/vr/tele-magic.vue
+++ portfolio/pages/works/vr/tele-magic.vue
@@ -8,12 +8,11 @@
       <p>
         自分から2~3mほど離れた場所に置いたThetaのライブストリーミング映像を見ながら、なんちゃってテレイグジスタンス体験ができるタワーディフェンスゲーム。<br>
         <br>
-        まずThetaに向かい合って座りHMDをかぶると正面にHMDをかぶっている現実の自分自身が見える。ゲームを開始すると現実の自分に向かって敵が進軍するので爆裂魔法を発生させて敵を撃退するという内容。<br>
+        まずThetaに向かい合って座りHMDをかぶると、HMDをかぶっている現実の自分自身が見える（Thetaの視点に乗り移る）。スタートボタンを押すと現実の自分に向かって敵が進軍するので、爆裂魔法を発生させて敵を撃退するというゲーム。<br>
         <br>
         爆裂魔法スイッチを注視するだけの簡単な操作だが、詠唱時間、スイッチの残数、自身のHP、敵のスピードなどを考慮して最適なタイミングで爆裂魔法を発生させないとクリアできないという戦略性も盛り込んだ。<br>
         <br>
-        Code<br>
-        <a href="https://github.com/mo49/ThetaLivestreaming" target="_blank">https://github.com/mo49/ThetaLivestreaming</a>
+        Code : <a href="https://github.com/mo49/ThetaLivestreaming" target="_blank">https://github.com/mo49/ThetaLivestreaming</a>
       </p>
     </div>
     <div class="work-image" data-bg="even">
@@ -21,21 +20,31 @@
       <ul class="work-image__list">
         <li class="work-image__item size-full">
           <div class="youtube">
-            <iframe src="https://www.youtube.com/embed/xN4hQIJo50s" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe src="https://www.youtube.com/embed/oVYiC1urFyQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
+          <caption>Theta背景あり</caption>
+        </li>
+        <li class="work-image__item size-full">
+          <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/HAP6bRYbTqs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+          <caption>Theta背景なし</caption>
         </li>
         <li class="work-image__item">
-
+          <img src="~assets/image/works/vr/tele-magic/2.jpg" @click="openModal">
+          <caption>
+            <a href="https://vracademy.jp/events/vrfes/" target="_blank">VRコンテンツfes. 2018 Spring</a> に出展
+          </caption>
         </li>
       </ul>
     </div>
     <div class="work-skill" data-bg="odd">
       <h2>Skill</h2>
-      <p>Maya</p>
+      <p>Unity / C# / FOVE / Theta</p>
     </div>
     <div class="work-time" data-bg="even">
       <h2>Production Time</h2>
-      <p>December, 2018. (about 2week)</p>
+      <p>February 2018. (about 2weeks)</p>
     </div>
     <div class="work-media" data-bg="odd">
     </div>
