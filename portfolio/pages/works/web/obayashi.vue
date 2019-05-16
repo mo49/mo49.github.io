d376a9d0bcfd076bf42ce4970c3c92af1a08fd1e
mo49 <k.mochizuki.0409@gmail.com>
2019-05-16 15:48:48 +0900

    add obayashi

---
+++ portfolio/pages/works/web/obayashi.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="container work" data-page="work">
+    <h1 class="work__title">
+      BIM Will MANAGEMENT SYSTEM
+    </h1>
+    <div class="work-about" data-bg="odd">
+      <h2>About</h2>
+      <p>
+        大林組の展示会で使用されたビル管理システムのデモサイト。
+        <br>
+        街の全景→異常を検知したビルにズームイン→異常を検知した設備の詳細を表示、という流れ。
+        <br>
+        <br>
+        私の担当は3Dモデルの表示と、カメラワーク、インタラクション全般。素材のBlenderデータのポリゴンを間引いたり、パーツごとに分解する作業に一番時間をかけた。オブジェクトの点滅表現はGLSLで書いている。
+        <br>
+      </p>
+
+    </div>
+    <div class="work-image" data-bg="even">
+      <h2>Image / Movie</h2>
+      <ul class="work-image__list">
+        <li class="work-image__item size-full">
+          <img src="~assets/image/works/web/kamakura-fireworks/2.png" @click="openModal">
+          <figcaption>
+            しだれ柳
+          </figcaption>
+        </li>
+        <li class="work-image__item size-full">
+          <div class="youtube">
+            <iframe src="https://www.youtube.com/embed/xfjxG1Ow9J4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="work-skill" data-bg="odd">
+      <h2>Skill</h2>
+      <p>Three.js / Blender / WebGL / JavaScript</p>
+    </div>
+    <div class="work-time" data-bg="even">
+      <h2>Production Time</h2>
+      <p>from July to September, 2016. (about 2months)</p>
+    </div>
+    <div class="work-media" data-bg="odd">
+    </div>
+    <modal ref="modal" />
+  </section>
+</template>
+
+<script>
+import Modal from "~/components/Modal"
+
+export default {
+  components: {
+    Modal,
+  },
+  methods: {
+    openModal(evt) {
+      this.$refs.modal.openModal(evt);
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+@import "~assets/sass/page/works";
+</style>
+
