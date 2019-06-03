ad4aaba374f8980b07ff96b97512565f833388e0
mo49 <k.mochizuki.0409@gmail.com>
2019-06-03 12:11:58 +0900

    英訳　tele-magic

--- portfolio/pages/works/vr/tele-magic.vue
+++ portfolio/pages/works/vr/tele-magic.vue
@@ -7,12 +7,27 @@
       <h2>About</h2>
       <p>
         自分から2~3mほど離れた場所に置いたThetaのライブストリーミング映像を見ながら、なんちゃってテレイグジスタンス体験ができるタワーディフェンスゲーム。<br>
-        <br>
+        <span class="en">
+          A tower defense game where you can experience a kind of telexistence experience while watching the live streaming video of Theta placed at a distance of 2 to 3 meters from you.
+        </span>
+      </p>
+      <p>
         まずThetaに向かい合って座りHMDをかぶると、HMDをかぶっている現実の自分自身が見える（Thetaの視点に乗り移る）。スタートボタンを押すと現実の自分に向かって敵が進軍するので、爆裂魔法を発生させて敵を撃退するというゲーム。<br>
-        <br>
+        <span class="en">
+          First, when you sit facing the Theta and wear the HMD, you will see you wearing the HMD in real world (transfer to Theta's point of view). Since the enemy will advance toward you in real world when you press the start button, you should generate explosion magic and repel the enemy.
+        </span>
+      </p>
+      <p>
         爆裂魔法スイッチを注視するだけの簡単な操作だが、詠唱時間、スイッチの残数、自身のHP、敵のスピードなどを考慮して最適なタイミングで爆裂魔法を発生させないとクリアできないという戦略性も盛り込んだ。<br>
-        <br>
+        <span class="en">
+          It is a simple operation that only looks at the bursting magic switch, but also requires the strategy that it can not be cleared unless the bursting magic is generated at an optimal timing considering the casting time, the remaining number of switches, its own HP, enemy speed, etc.
+        </span>
+      </p>
+      <p>
         Code : <a class="link-hover" href="https://github.com/mo49/ThetaLivestreaming" target="_blank">https://github.com/mo49/ThetaLivestreaming</a>
+        <span class="en">
+
+        </span>
       </p>
     </div>
     <div class="work-image" data-bg="even">
