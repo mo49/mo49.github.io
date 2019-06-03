c633058c1fb8a94d50424848b95f4ab4769d9d73
mo49 <k.mochizuki.0409@gmail.com>
2019-06-03 11:51:10 +0900

    英訳

--- portfolio/pages/works/3dcg-vfx/showroom-office.vue
+++ portfolio/pages/works/3dcg-vfx/showroom-office.vue
@@ -1,14 +1,24 @@
 <template>
   <section class="container work" data-page="work">
     <h1 class="work__title">
-      株式会社SHOWROOM,荒井ビルオフィス
+      株式会社SHOWROOM,荒井ビルオフィス<br>
+      <span class="en">
+        SHOWROOM Inc. Office in Arai Building
+      </span>
     </h1>
     <div class="work-about" data-bg="odd">
       <h2>About</h2>
       <p>
         デジタルハリウッド3DCGコースに入学から3ヶ月後の課題制作。近い将来VR出社するのが当たり前になるかもしれないと考え、より具体的なイメージを掴むため弊社オフィスを3DCGで再現してみた。<br>
-        <br>
-        約30のスポットライトとポイントライトを仕込んだり、スタンドライトにSSSで透け感を出してみたりと、ライティング周りに時間を割いた。Arnoldレンダリング。スーパースキャンスタジオで撮影した人体モデル以外をモデリングした。
+        <span class="en">
+          Produced a work three months after entering Digital Hollywood 3DCG course. I thought that it might be commonplace to work in VR in the near future, and reproduced my office with 3DCG to get a more specific image.
+        </span>
+      </p>
+      <p>
+        約30のスポットライトとポイントライトを仕込んだり、スタンドライトにSSSで透け感を出してみたりと、ライティング周りに時間を割いた。Arnoldレンダリング。スーパースキャンスタジオで撮影した人体モデル以外をモデリングした。<br>
+        <span class="en">
+          I used about 30 spotlights and point lights, and tried to make the standlights see-through with SSS. So I spent some time around the lighting using Arnold. I modeled everything other than the human body model shot at Super Scan Studio.
+        </span>
       </p>
     </div>
     <div class="work-image" data-bg="even">
