73a2d197cfe4a15ca648f669dae8d60ace2bdcba
mo49 <k.mochizuki.0409@gmail.com>
2019-07-11 11:36:28 +0900

    add pop advect

--- portfolio/pages/gists.vue
+++ portfolio/pages/gists.vue
@@ -14,6 +14,7 @@ export default {
   data() {
     return {
       ids: [
+        '8f043feeeeb710abe701314d6bf34a64',
         'afedd914135ac082e257c420dc374a6b',
         '386d21550624e25951da2a9ff4bf9064',
         '1bb5888ee47063412bf60798b04a5a32',
