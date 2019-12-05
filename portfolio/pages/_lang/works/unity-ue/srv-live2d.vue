2160770eda9d832e41462b4163c97aa73fddef14
mo49 <k.mochizuki.0409@gmail.com>
2019-12-05 17:28:29 +0900

    cg/vrの多言語対応

--- portfolio/pages/_lang/works/unity-ue/srv-live2d.vue
+++ portfolio/pages/_lang/works/unity-ue/srv-live2d.vue
@@ -1,22 +1,9 @@
 <template>
   <section class="container work" data-page="work">
-    <h1 class="work__title">
-      SHOWROOM V - Readyyy!版
-    </h1>
+    <h1 class="work__title">{{ $t('works.readyyy.title') }}</h1>
     <div class="work-about" data-bg="odd">
       <h2>{{ $t('works.headlines.about') }}</h2>
-      <p>
-        誰でもスマホ一台でバーチャル配信ができる既存のアプリ<a class="link-hover" href="https://itunes.apple.com/jp/app/showroom-v/id1440259867" target="_blank" rel="noopener noreferrer">SHOWROOM V</a>のLive2D版の作成。バーチャル配信者はVRMというフォーマットでモデルを作ることが多いが、実際には二次元のイラストで代用している人も多くLive2Dへの需要があったため対応した。案件ごとにカスタマイズしたアプリをオーガナイザー様に配布していて、今回は（株）セガのアイドル育成アプリゲーム『Readyyy! 』の演者にSHOWROOM配信してもらうためのカスタマイズを行った。2018年12月以降、各メンバーに定期的に配信してもらい、その都度背景やキャラの修正を加えるなどのメンテナンスを施している。<br>
-        <span class="en">
-          Live2D version of the application, SHOWROOM V that enables a user to live stream as virtual character on smartphone. Virtual Livestreamers often make models in the format of VRM, but in reality, many people are using two-dimensional illustrations instead, so there is a demand for Live2D. We distributed an app customized for each organizer, and this time, I customized our app to have performers of SEGA's new idol project “Readyyy!” live stream on SHOWROOM. Since December 2018, each member regularly live stream, and I maintain backgrounds and characters each time.
-        </span>
-      </p>
-      <p>
-        私の担当はLive2Dへの移行作業から運用までのすべて。トラッキングのコードを丸ごとVRM用からLive2D用に差し替えるにあたり、ライブラリはULSeeを使用。取得した頂点情報から演者がもっとも自然に動くようにパラメータの閾値やトラッキングの感度などを調整した。大量にアクセスがあった際にパフォーマンスが落ちたため、メモリ、キャッシュ、ドローコール、テクスチャベイクあたりを見直して、チューニング作業を行った。<br>
-        <span class="en">
-          I’m in charge of programming and management. The library, ULSee is adopted to replace the tracking code entirely for VRM with for Live2D. Based on the acquired vertex information, the parameter threshold and tracking sensitivity were adjusted so that the performer moved most naturally. Performance was not good when there was a lot of access, so I tuned that I reviewed the memory, cache, draw calls and texture bakes.
-        </span>
-      </p>
+      <pre>{{ $t('works.readyyy.desc') }}</pre>
     </div>
     <div class="work-image" data-bg="even">
       <h2>{{ $t('works.headlines.image') }}</h2>
@@ -30,7 +17,7 @@
           <div class="youtube">
             <iframe src="https://www.youtube.com/embed/zeGwJnqN_k4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
-          <figcaption>スマホから見た配信中画面のキャプチャ</figcaption>
+          <figcaption v-if="$isJa()">スマホから見た配信中画面のキャプチャ</figcaption>
         </li>
       </ul>
     </div>
@@ -40,7 +27,7 @@
     </div>
     <div class="work-time" data-bg="even">
       <h2>{{ $t('works.headlines.production-time') }}</h2>
-      <p>from October to December 2018. (about 3months)</p>
+      <p>{{ $t('works.readyyy.production-time') }}</p>
     </div>
     <div class="work-media" data-bg="odd">
       <h2>Media</h2>
--- portfolio/pages/_lang/works/vr/tele-magic.vue
+++ portfolio/pages/_lang/works/vr/tele-magic.vue
@@ -1,34 +1,10 @@
 <template>
   <section class="container work" data-page="work">
-    <h1 class="work__title">
-      Tele-Magic
-    </h1>
+    <h1 class="work__title">{{ $t('works.tele-magic.title') }}</h1>
     <div class="work-about" data-bg="odd">
       <h2>{{ $t('works.headlines.about') }}</h2>
-      <p>
-        自分から2~3mほど離れた場所に置いたThetaのライブストリーミング映像を見ながら、なんちゃってテレイグジスタンス体験ができるタワーディフェンスゲーム。<br>
-        <span class="en">
-          A tower defense game where you can experience a kind of telexistence experience while watching the live streaming video of Theta placed at a distance of 2 to 3 meters from you.
-        </span>
-      </p>
-      <p>
-        まずThetaに向かい合って座りHMDをかぶると、HMDをかぶっている現実の自分自身が見える（Thetaの視点に乗り移る）。スタートボタンを押すと現実の自分に向かって敵が進軍するので、爆裂魔法を発生させて敵を撃退するというゲーム。<br>
-        <span class="en">
-          First, when you sit facing the Theta and wear the HMD, you will see you wearing the HMD in real world (transfer to Theta's point of view). Since the enemy will advance toward you in real world when you press the start button, you should generate explosion magic and repel the enemy.
-        </span>
-      </p>
-      <p>
-        爆裂魔法スイッチを注視するだけの簡単な操作だが、詠唱時間、スイッチの残数、自身のHP、敵のスピードなどを考慮して最適なタイミングで爆裂魔法を発生させないとクリアできないという戦略性も盛り込んだ。<br>
-        <span class="en">
-          It is a simple operation that only looks at the bursting magic switch, but also requires the strategy that it can not be cleared unless the bursting magic is generated at an optimal timing considering the casting time, the remaining number of switches, its own HP, enemy speed, etc.
-        </span>
-      </p>
-      <p>
-        Code : <a class="link-hover" href="https://github.com/mo49/ThetaLivestreaming" target="_blank">https://github.com/mo49/ThetaLivestreaming</a>
-        <span class="en">
-
-        </span>
-      </p>
+      <pre>{{ $t('works.tele-magic.desc') }}</pre>
+      <p>Code : <a class="link-hover" href="https://github.com/mo49/ThetaLivestreaming" target="_blank">https://github.com/mo49/ThetaLivestreaming</a></p>
     </div>
     <div class="work-image" data-bg="even">
       <h2>{{ $t('works.headlines.image') }}</h2>
@@ -37,19 +13,17 @@
           <div class="youtube">
             <iframe src="https://www.youtube.com/embed/oVYiC1urFyQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
-          <figcaption>Theta背景あり</figcaption>
+          <figcaption>{{ $t('works.tele-magic.cap1') }}</figcaption>
         </li>
         <li class="work-image__item size-full">
           <div class="youtube">
             <iframe src="https://www.youtube.com/embed/HAP6bRYbTqs" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
-          <figcaption>Theta背景なし</figcaption>
+          <figcaption>{{ $t('works.tele-magic.cap2') }}</figcaption>
         </li>
         <li class="work-image__item">
           <img src="~assets/image/works/vr/tele-magic/2.jpg" @click="openModal">
-          <figcaption>
-            <a class="link-hover" href="https://vracademy.jp/events/vrfes/" target="_blank">VRコンテンツfes. 2018 Spring</a> に出展
-          </figcaption>
+          <figcaption>{{ $t('works.tele-magic.cap3') }}</figcaption>
         </li>
       </ul>
     </div>
@@ -59,7 +33,7 @@
     </div>
     <div class="work-time" data-bg="even">
       <h2>{{ $t('works.headlines.production-time') }}</h2>
-      <p>February 2018. (about 2weeks)</p>
+      <p>{{ $t('works.tele-magic.production-time') }}</p>
     </div>
     <div class="work-media" data-bg="odd">
     </div>
