62a7491b4d0ffa7c9ead2452488de137779da471
mo49 <k.mochizuki.0409@gmail.com>
2022-04-09 15:30:30 +0900

    head

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,17 @@ import i18n from './nuxt-i18n.config'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  head: {
+    title: '%s | Portfolio Site',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' },
+      { hid: 'description', name: 'description', content: 'Mochizuki Kazuki のポートフォリオサイトです' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
   buildModules: [
     '@nuxtjs/stylelint-module',
     '@nuxtjs/style-resources',
